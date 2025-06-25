d213d64c18208e7b4eef8f799702d8957a39f04c
vgt1 <vgt1@sfu.ca>
2025-06-25 12:28:39 -0700

    Additional fixes

--- emulator.c
+++ emulator.c
@@ -53,9 +53,9 @@ void execute_rtype(Instruction instruction, Processor *processor) {
             switch (instruction.rtype.funct7) {
                 case 0x0:
                     // Add
-                    processor->R[instruction.rtype.rd] =
-                        ((sWord)processor->R[instruction.rtype.rs1]) +
-                        ((sWord)processor->R[instruction.rtype.rs2]);
+                     processor->R[instruction.rtype.rd] =               //  Access the "instruction.rtype.rd" indexed member of the Processor's array of registers (as a signed 32-bit number)Add commentMore actions
+                        ((sWord)processor->R[instruction.rtype.rs1]) + //  Access the "instruction.rtype.rs1" indexed member of the Processor's array of registers (as a signed 32-bit number)
+                        ((sWord)processor->R[instruction.rtype.rs2]);  //  Access the "instruction.rtype.rs2" indexed member of the Processor's array of registers (as a signed 32-bit number)
                     break;
                 case 0x1:
                     // Mul
@@ -334,48 +334,67 @@ void execute_branch(Instruction instruction, Processor *processor) {
 }
 
 void execute_load(Instruction instruction, Processor *processor, Byte *memory) {
+    Address offsetAddress = (processor->R[instruction.itype.rs1]) + instruction.itype.imm;   // Calculate offsetted address for load (with base address in rs1)
     switch (instruction.itype.funct3) {
-        /* YOUR CODE HERE */
+        case 0x0:
+            processor->R[instruction.itype.rd] = sign_extend_number(load(memory, offsetAddress, LENGTH_BYTE), 8);  // Load in signed extended byte from offsetted address
+            break;
+        case 0x1:
+            processor->R[instruction.itype.rd] = sign_extend_number(load(memory, offsetAddress, LENGTH_HALF_WORD), 16);  // Load in signed extended half-word from offsetted address
+            break;
+        case 0x2:
+            processor->R[instruction.itype.rd] = load(memory, offsetAddress, LENGTH_WORD);  // Load in word from offsetted address
+            break;
         default:
             handle_invalid_instruction(instruction);
             break;
     }
+    // update PC
+    processor->PC += 4;
 }
 
 void execute_store(Instruction instruction, Processor *processor, Byte *memory) {
+    Address offsetAddress = (processor->R[instruction.stype.rs1]) + get_store_offset(instruction);   // Calculate offsetted address for load (with base address in rs1)
     switch (instruction.stype.funct3) {
-        /* YOUR CODE HERE */
+        case 0x0:
+            store(memory, offsetAddress, LENGTH_BYTE, processor->R[instruction.stype.rs2]);
+            break;
+        case 0x1:
+            store(memory, offsetAddress, LENGTH_HALF_WORD, processor->R[instruction.stype.rs2]);
+            break;
+        case 0x2:
+            store(memory, offsetAddress, LENGTH_WORD, processor->R[instruction.stype.rs2]);
+            break;
         default:
             handle_invalid_instruction(instruction);
             exit(-1);
             break;
     }
+    // update PC
+    processor->PC += 4;
 }
 
 void execute_jal(Instruction instruction, Processor *processor) {
-    /* YOUR CODE HERE */
+    processor->R[instruction.ujtype.rd] = (processor->PC + 4);
+    processor->PC += get_jump_offset(instruction);
 }
 
 void execute_lui(Instruction instruction, Processor *processor) {
-    /* YOUR CODE HERE */
-}
+    processor->R[instruction.utype.rd] = instruction.utype.imm << 12;
+    // update PC
+    processor->PC += 4;}
 
 void store(Byte *memory, Address address, Alignment alignment, Word value) {
     if(alignment == LENGTH_BYTE) {
-
-        memory[address] = value & 0xFF;
-
+         memory[address] = value & ((1U << 8) - 1);
     } else if(alignment == LENGTH_HALF_WORD) {
-
-        memory[address] = value & 0xFF;
-        memory[address + 1] = (value >> 8) & 0xFF;
-
+        memory[address] = value & ((1U << 8) - 1);
+        memory[address+1] = (value << 8) & ((1U << 8) - 1);  // Address +1 is address with 1 byte offset
     } else if(alignment == LENGTH_WORD) {
-
-        memory[address] = value & 0xFF;
-        memory[address + 1] = (value >> 8) & 0xFF;
-        memory[address + 2] = (value >> 16) & 0xFF;
-        memory[address + 3] = (value >> 24) & 0xFF;
+        memory[address] = value & ((1U << 8) - 1);
+        memory[address+1] = (value << 8) & ((1U << 8) - 1);
+        memory[address+2] = (value << 16) & ((1U << 8) - 1);
+        memory[address+3] = (value << 24) & ((1U << 8) - 1);
         
     } else {
         printf("Error: Unrecognized alignment %d\n", alignment);
@@ -387,7 +406,7 @@ Word load(Byte *memory, Address address, Alignment alignment) {
     if(alignment == LENGTH_BYTE) {
         return memory[address];
     } else if(alignment == LENGTH_HALF_WORD) {
-        return (memory[address+1] << 8) + memory[address];
+         return (memory[address+1] << 8) + memory[address];  // Address +1 is address with 1 byte offset
     } else if(alignment == LENGTH_WORD) {
         return (memory[address+3] << 24) + (memory[address+2] << 16)
                + (memory[address+1] << 8) + memory[address];
